81048f1fc76a6fe0c837598a47ce9663d1b5df84
Rafeal Freitas <raffreitasx@outlook.com>
2025-04-18 15:28:30 -0300

    Add .env.example and .gitignore files; implement Database struct with connection methods

---
+++ framework/database/db.go
@@ -0,0 +1,68 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	Db            *gorm.DB
+	Dsn           string
+	DsnTest       string
+	DbType        string
+	DbTypeTest    string
+	Debug         bool
+	AutoMigrateDb bool
+	Env           string
+}
+
+func NewDb() *Database {
+	return &Database{}
+}
+
+func NewDbTest() *gorm.DB {
+	dbInstance := NewDb()
+	dbInstance.Env = "Test"
+	dbInstance.DbTypeTest = "sqlite3"
+	dbInstance.DsnTest = ":memory"
+	dbInstance.AutoMigrateDb = true
+	dbInstance.Debug = true
+
+	connection, err := dbInstance.Connect()
+
+	if err != nil {
+		log.Fatalf("Test db error: %v", err)
+	}
+
+	return connection
+}
+
+func (d *Database) Connect() (*gorm.DB, error) {
+	var err error
+
+	if d.Env != "Test" {
+		d.Db, err = gorm.Open(postgres.Open(d.Dsn), &gorm.Config{})
+	} else {
+		d.Db, err = gorm.Open(sqlite.Open(d.DsnTest), &gorm.Config{})
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if d.Debug {
+		d.Db.Logger.LogMode(1)
+	}
+
+	if d.AutoMigrateDb {
+		err = d.Db.AutoMigrate()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return d.Db, nil
+}
