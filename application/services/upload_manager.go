826e726e4b008a63ec9342e6d84c45d133af672c
Rafeal Freitas <raffreitasx@outlook.com>
2025-04-18 20:03:42 -0300

    Add VideoUpload service for handling video uploads to S3 with concurrency support

---
+++ application/services/upload_manager.go
@@ -0,0 +1,138 @@
+package services
+
+import (
+	"context"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type VideoUpload struct {
+	Paths        []string
+	VideoPath    string
+	OutputBucket string
+	Errors       []string
+}
+
+func NewVideoUpload() *VideoUpload {
+	return &VideoUpload{}
+}
+
+func (vu *VideoUpload) UploadObject(objectPath string, client *s3.Client, ctx context.Context) error {
+	path := strings.Split(objectPath, os.TempDir())
+
+	f, err := os.Open(objectPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: &vu.OutputBucket,
+		Key:    &path[1],
+		Body:   f,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (vu *VideoUpload) LoadPaths() error {
+
+	err := filepath.Walk(vu.VideoPath, func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() {
+			vu.Paths = append(vu.Paths, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
+	in := make(chan int, runtime.NumCPU())
+	returnChannel := make(chan string)
+
+	err := vu.LoadPaths()
+	if err != nil {
+		return err
+	}
+
+	client, ctx, err := getClientUpload()
+	if err != nil {
+		return err
+	}
+
+	for range concurrency {
+		go vu.uploadWorker(in, returnChannel, client, ctx)
+	}
+
+	go func() {
+		for x := range vu.Paths {
+			in <- x
+		}
+		close(in)
+	}()
+
+	for r := range returnChannel {
+		if r != "" {
+			doneUpload <- r
+			break
+		}
+	}
+
+	return nil
+}
+
+func (vu *VideoUpload) uploadWorker(in chan int, returnChan chan string, uploadClient *s3.Client, ctx context.Context) {
+	for x := range in {
+		err := vu.UploadObject(vu.Paths[x], uploadClient, ctx)
+		if err != nil {
+			vu.Errors = append(vu.Errors, vu.Paths[x])
+			log.Printf("error during the upload: %v. error: %v", vu.Paths[x], err)
+			returnChan <- err.Error()
+		}
+
+		returnChan <- ""
+	}
+
+	returnChan <- "uploaded completed"
+}
+
+func getClientUpload() (*s3.Client, context.Context, error) {
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	s3Options := s3.Options{
+		Region:      cfg.Region,
+		Credentials: cfg.Credentials,
+	}
+
+	awsEndpoint := os.Getenv("AWS_ENDPOINT")
+	if awsEndpoint != "" {
+		s3Options.BaseEndpoint = aws.String(awsEndpoint)
+		s3Options.UsePathStyle = true
+	}
+
+	client := s3.New(s3Options)
+
+	return client, ctx, nil
+}
