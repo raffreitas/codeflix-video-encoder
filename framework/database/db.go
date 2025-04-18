8c37d96ecfa8809b766203178f0df43bdd4706ef
Rafeal Freitas <raffreitasx@outlook.com>
2025-04-18 15:54:52 -0300

    Fix environment string case in NewDbTest and Connect functions

--- framework/database/db.go
+++ framework/database/db.go
@@ -26,7 +26,7 @@ func NewDb() *Database {
 
 func NewDbTest() *gorm.DB {
 	dbInstance := NewDb()
-	dbInstance.Env = "Test"
+	dbInstance.Env = "test"
 	dbInstance.DbTypeTest = "sqlite3"
 	dbInstance.DsnTest = ":memory"
 	dbInstance.AutoMigrateDb = true
@@ -44,7 +44,7 @@ func NewDbTest() *gorm.DB {
 func (d *Database) Connect() (*gorm.DB, error) {
 	var err error
 
-	if d.Env != "Test" {
+	if d.Env != "test" {
 		d.Db, err = gorm.Open(postgres.Open(d.Dsn), &gorm.Config{})
 	} else {
 		d.Db, err = gorm.Open(sqlite.Open(d.DsnTest), &gorm.Config{})
