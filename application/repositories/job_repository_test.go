d0f6cc2c8dcb9fdb9aa184fee67bcd8706f36a86
Rafeal Freitas <raffreitasx@outlook.com>
2025-04-18 16:05:14 -0300

    Implement JobRepository and corresponding tests for database operations

---
+++ application/repositories/job_repository_test.go
@@ -0,0 +1,68 @@
+package repositories_test
+
+import (
+	"testing"
+	"time"
+	"video-encoder/application/repositories"
+	"video-encoder/domain"
+	"video-encoder/framework/database"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJobRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewString()
+	video.FilePath = "fake_path"
+	video.CreatedAt = time.Now()
+
+	job, err := domain.NewJob("output_path", "Pending", video)
+
+	require.Nil(t, err)
+	require.NotEmpty(t, job.ID)
+
+	repoJob := repositories.JobRepositoryDb{Db: db}
+	repoJob.Insert(job)
+
+	j, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.ID, job.ID)
+	require.Equal(t, j.VideoID, video.ID)
+}
+
+func TestJobRepositoryDbUpdate(t *testing.T) {
+	db := database.NewDbTest()
+
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewString()
+	video.FilePath = "fake_path"
+	video.CreatedAt = time.Now()
+
+	job, err := domain.NewJob("output_path", "Pending", video)
+
+	require.Nil(t, err)
+	require.NotEmpty(t, job.ID)
+	require.Equal(t, job.OutputBucketPath, "output_path")
+
+	repoJob := repositories.JobRepositoryDb{Db: db}
+	repoJob.Insert(job)
+
+	job.Status = "Complete"
+
+	repoJob.Update(job)
+
+	j, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.Status, job.Status)
+}
