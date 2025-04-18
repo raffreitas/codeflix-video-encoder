f1f00a6ba048b2f3bf82e5674127aa682c984eed
Rafeal Freitas <raffreitasx@outlook.com>
2025-04-18 17:07:09 -0300

    Refactor LocalStack setup: update S3 bucket path and consolidate environment variables

--- .docker/localstack/init-localstack.sh
+++ .docker/localstack/init-localstack.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Set AWS credentials for LocalStack
+export AWS_ACCESS_KEY_ID=localstack
+export AWS_SECRET_ACCESS_KEY=localstack
+export AWS_DEFAULT_REGION=us-east-1
+
 echo "Esperando o LocalStack iniciar..."
 sleep 10
 
 echo "Criando bucket S3..."
-aws --endpoint-url=http://localhost:4566 s3 mb s3://codeflix-videos --region us-east-1
+aws --endpoint-url=http://localstack:4566 s3 mb s3://codeflix-videos
 
 echo "Listando buckets S3..."
-aws --endpoint-url=http://localhost:4566 s3 ls
+aws --endpoint-url=http://localstack:4566 s3 ls
 
 echo "Configuração do LocalStack concluída!"
